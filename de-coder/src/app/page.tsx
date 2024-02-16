dd1e6365f448867c86627228bef137d265f83136
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-17 00:11:52 +0530

    Created next app with shadcn ui

---
+++ de-coder/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+const HomePage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>MakeYourPizza</title>
+      </Head>
+    </div>
+  );
+};
+
+export default HomePage;
