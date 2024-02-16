8ef4c6bbeff9fba88228d3205b9f1fa604d63d06
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-16 22:45:06 +0530

    Initialized react app with typescript material tailwind and tailwind css

--- pizza-delivery-client/src/App.tsx
+++ pizza-delivery-client/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@material-tailwind/react";
 
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Landing from "./components/Landing";
+
+const Layout = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>{/* <Route path="/" element={<Landing />}></Route> */}</Routes>
+    </>
   );
-}
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <ThemeProvider>
+        <Layout />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ pizza-delivery-client/tailwind.config.tsx
@@ -0,0 +1,27 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+export default withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pizza: {
+          50: "#fff5e6", // light crust
+          100: "#ffe0b3", // crust
+          200: "#ffcc80", // light cheese
+          300: "#ffb74d", // cheese
+          400: "#ffa726", // light sauce
+          500: "#ff9800", // sauce
+          600: "#f57c00", // light topping
+          700: "#ef6c00", // topping
+          800: "#e65100", // dark topping
+          900: "#d84315", // burnt edge
+        },
+      },
+    },
+  },
+  plugins: [],
+});
