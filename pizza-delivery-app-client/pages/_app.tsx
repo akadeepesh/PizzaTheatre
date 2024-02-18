d42b4ca447fa2a253fb54fd773f6c1c25da2dea3
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-02-18 13:26:13 +0530

    base theme neobrutalism

--- pizza-delivery-app-client/pages/_app.tsx
+++ pizza-delivery-app-client/pages/_app.tsx
@@ -1,22 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from '@clerk/themes';
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { neobrutalism } from "@clerk/themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
     <>
       <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
         <ClerkProvider
           appearance={{
-            baseTheme: dark
+            baseTheme: neobrutalism,
           }}
         >
-            <Component {...pageProps} />
+          <Component {...pageProps} />
         </ClerkProvider>
       </NextThemesProvider>
     </>
-  )
+  );
 }
