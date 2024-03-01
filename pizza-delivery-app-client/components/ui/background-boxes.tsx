23f43fd5831fba2f8a21e798f3f0cc665a51c904
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-02 03:19:10 +0530

    edited background boxes

--- pizza-delivery-app-client/components/ui/background-boxes.tsx
+++ pizza-delivery-app-client/components/ui/background-boxes.tsx
@@ -35,7 +35,7 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
       {rows.map((_, i) => (
         <motion.div
           key={`row` + i}
-          className="w-16 h-8  border-l  border-slate-700 relative"
+          className="w-16 h-8 border-l  border-primary relative"
         >
           {cols.map((_, j) => (
             <motion.div
@@ -47,7 +47,7 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
                 transition: { duration: 2 },
               }}
               key={`col` + j}
-              className="w-16 h-8  border-r border-t border-slate-700 relative"
+              className="w-16 h-8  border-r border-t border-primary relative"
             >
               {j % 2 === 0 && i % 2 === 0 ? (
                 <svg
@@ -56,7 +56,7 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  className="absolute h-6 w-10 -top-[14px] -left-[22px] text-slate-700 stroke-[1px] pointer-events-none"
+                  className="absolute h-6 w-10 -top-[14px] -left-[22px] text-primary stroke-[1px] pointer-events-none"
                 >
                   <path
                     strokeLinecap="round"
