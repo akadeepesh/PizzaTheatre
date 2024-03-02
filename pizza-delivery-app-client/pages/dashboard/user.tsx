d7f15dd84883f3723db403ce0702be1c5607949d
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-02 23:59:40 +0530

    User Profile Created

--- pizza-delivery-app-client/pages/dashboard/user.tsx
+++ pizza-delivery-app-client/pages/dashboard/user.tsx
@@ -1,7 +1,25 @@
+import { UserProfile } from "@clerk/nextjs";
+import { dark, neobrutalism } from "@clerk/themes";
 import React from "react";
+import { useTheme } from "next-themes";
+import Head from "next/head";
+import { useUser } from "@clerk/nextjs";
 
 const User = () => {
-  return <div>user</div>;
+  const { theme } = useTheme();
+  const { user } = useUser();
+  return (
+    <div className="flex flex-wrap justify-center max-w-screen-lg mx-auto my-20 sm:my-24 md:my-28 lg:my-36">
+      <Head>
+        <title>{user?.fullName}</title>
+      </Head>
+      <UserProfile
+        appearance={{
+          baseTheme: theme === "dark" ? dark : neobrutalism,
+        }}
+      />
+    </div>
+  );
 };
 
 export default User;
