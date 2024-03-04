7bc100a6abc364048226958028a578f14713d4bb
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-05 02:53:33 +0530

    Styled menu page

--- pizza-delivery-app-client/pages/menu.tsx
+++ pizza-delivery-app-client/pages/menu.tsx
@@ -99,11 +99,11 @@ export function Items() {
   return (
     <div className="flex flex-wrap justify-center max-w-screen-xl mx-auto mt-20 sm:mt-24 md:mt-28 lg:mt-36">
       {pizzas.map((pizza, index) => (
-        <CardContainer key={index} className="inter-var mx-5 my-5">
+        <CardContainer key={index} className="m-5">
           <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[24rem] h-auto rounded-xl p-6 border">
             <CardItem
               translateZ="50"
-              className="text-xl flex flex-row justify-between font-bold text-neutral-600 dark:text-white"
+              className="text-xl font-Anta flex flex-row justify-between font-bold text-neutral-600 dark:text-white"
             >
               <div className="">{pizza.name}</div>
               <div className="">₹ {pizza.price}</div>
@@ -111,7 +111,7 @@ export function Items() {
             <CardItem
               as="p"
               translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              className="text-neutral-500 font-Annapura text-sm max-w-sm mt-2 dark:text-neutral-300"
             >
               {pizza.toppings}
             </CardItem>
