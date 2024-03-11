e2c0226182370165e2bb464dd00bfc2012f51929
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-12 02:06:47 +0530

    Trying for role based RBAC Role based access control

--- pizza-delivery-app-client/convex/pizzas.ts
+++ pizza-delivery-app-client/convex/pizzas.ts
@@ -12,6 +12,22 @@ export const pizza = mutation({
     quantity: v.number(),
   },
   async handler(ctx, args) {
+    function validatePrice(price: { small?: number; medium?: number }) {
+      if (!price.small && !price.medium) {
+        throw new Error(
+          "At least one of small or medium price must be provided."
+        );
+      }
+      if (
+        (price.small && price.small < 0) ||
+        (price.medium && price.medium < 0)
+      ) {
+        throw new Error("Pizza price cannot be negative.");
+      }
+    }
+
+    validatePrice(args.price);
+
     await ctx.db.insert("pizza", {
       name: args.name,
       toppings: args.toppings,
--- pizza-delivery-app-client/next.config.js
+++ pizza-delivery-app-client/next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
     domains: ["images.unsplash.com"],
   },
   reactStrictMode: true,
+  // webpack(config) {
+  //   config.resolve.fallback = {
+
+  //     // if you miss it, all the other options in fallback, specified
+  //     // by next.js will be dropped.
+  //     ...config.resolve.fallback,  
+
+  //     fs: false, // the solution of could not found module 'fs'
+  //   };
+    
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
