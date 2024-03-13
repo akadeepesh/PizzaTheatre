a41eba59e42778a1538d190aa19e323b70efe9a8
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-14 02:23:56 +0530

    Added user db logic

---
+++ pizza-delivery-app-client/convex/users.ts
@@ -0,0 +1,15 @@
+// import { v } from "convex/values";
+// import { mutation } from "./_generated/server";
+// import { pizza } from "./pizzas";
+
+// export const user = mutation({
+//   args: {
+//     username: v.string(),
+//     isAdmin: v.boolean(),
+//   },
+//   async handler(ctx, args) {
+//     await ctx.db.insert("user", {
+//       username: args.username,
+//     });
+//   },
+// });
--- pizza-delivery-app-client/pages/admin/add-new-pizza.tsx
+++ pizza-delivery-app-client/pages/admin/add-new-pizza.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import AddNewPizza from "@/components/AddNewPizza";
 import Head from "next/head";
-// import { useUser } from "@clerk/nextjs";
 
 const AddPizza = () => {
-  // const user = useUser();
   return (
     <div>
       <Head>
