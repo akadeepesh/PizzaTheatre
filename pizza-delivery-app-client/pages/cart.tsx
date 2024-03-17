5ccb054ab0d4e0dbb6c915148335325c25e020b7
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2024-03-17 17:17:26 +0530

    SEO in cart page and add to cart logic completion in menu

--- pizza-delivery-app-client/pages/cart.tsx
+++ pizza-delivery-app-client/pages/cart.tsx
@@ -5,16 +5,29 @@ import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { NextSeo } from "next-seo";
 import { useUser } from "@clerk/nextjs";
 
 const Cart = () => {
-  const { user } = useUser();
+  const { user, isLoaded } = useUser();
   const cart = useQuery(api.cart.getUserCartItems, { userId: user?.id || "" });
   const pizzass = useQuery(api.pizzas.getPizzas);
 
   return (
     <div className="flex flex-col md:flex-row justify-between items-center max-w-screen-lg mx-auto mt-20 sm:mt-24 md:mt-28 lg:mt-36">
       <div className="w-3/5 h-full">
+        <NextSeo
+          title={isLoaded ? `${user?.fullName} | Cart` : "Loading..."}
+          description="Pizza Theater Cart, where you can see and manage all the items you have added to your cart."
+          canonical="https://pizza-theater.vercel.app/"
+          openGraph={{
+            url: "https://pizza-theater.vercel.app/",
+            title: "Pizza Theater",
+            description:
+              "Pizza Theater is an Pizza ordering app that allows you to order your favorite pizza from the comfort of your home.",
+            siteName: "Pizza Theater",
+          }}
+        />
         {cart?.map((cartItem) => {
           const pizza = pizzass?.find((p) => p._id === cartItem.pizzaId);
           return pizza ? (
@@ -81,75 +94,5 @@ const Cart = () => {
     </div>
   );
 };
-
-// return (
-//   <div className="">
-//     <div className="flex flex-col md:flex-row justify-between items-center max-w-screen-lg mx-auto mt-20 sm:mt-24 md:mt-28 lg:mt-36">
-//       <div className="w-3/5 h-full">
-//         {cart?.map((pizza, index) => (
-//           <CardContainer className="inter-var w-full">
-//             <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-5/6 h-auto rounded-xl p-6 border">
-//               <CardItem
-//                 translateZ="50"
-//                 className="text-xl flex flex-row justify-between font-bold text-neutral-600 dark:text-white"
-//               >
-//                 <div className="">{pizza.pizzaId}</div>
-//                 <div className="">₹ 10</div>
-//               </CardItem>
-//               <CardItem
-//                 as="p"
-//                 translateZ="60"
-//                 className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-//               >
-//                 Chees hi Chees
-//               </CardItem>
-//               <CardItem translateZ="100" className="w-full mt-4">
-//                 <Image
-//                   // src={images[index]}
-//                   src={
-//                     "https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//                   }
-//                   height="1000"
-//                   width="1000"
-//                   className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-//                   alt="thumbnail"
-//                 />
-//               </CardItem>
-//               <div className="flex justify-between items-center mt-20 select-none">
-//                 <CardItem
-//                   translateZ={20}
-//                   as="button"
-//                   className="rounded-2xl font-normal dark:text-white"
-//                 >
-//                   Select Type
-//                 </CardItem>
-//                 <CardItem
-//                   translateZ={20}
-//                   as="button"
-//                   className="rounded-2xl font-normal dark:text-white"
-//                 >
-//                   Value
-//                 </CardItem>
-//                 <CardItem
-//                   translateZ={20}
-//                   as="button"
-//                   className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-//                 >
-//                   Checkout
-//                 </CardItem>
-//               </div>
-//             </CardBody>
-//           </CardContainer>
-//         ))}
-//         <Separator className="bg-primary my-20" />
-//       </div>
-//       <div className="mockup-window border border-base-300 md:mt-20 w-5/6 md:w-1/3 h-full">
-//         <div className="flex justify-center border border-base-300 px-4 py-16 bg-secondary">
-//           Buy Menu
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// );
 
 export default Cart;
--- pizza-delivery-app-client/pages/menu.tsx
+++ pizza-delivery-app-client/pages/menu.tsx
@@ -1,12 +1,11 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { Separator } from "@/components/ui/separator";
-import Cart from "@/components/items";
 import Head from "next/head";
 
-import { Minus, Plus } from "lucide-react";
+import { Minus, Plus, Trash2 } from "lucide-react";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useMutation } from "convex/react";
@@ -20,157 +19,54 @@ import {
 } from "@/components/ui/select";
 import { useUser } from "@clerk/nextjs";
 
-export interface Pizza {
-  name: string;
-  toppings: string;
-  price: string;
-}
-
-const pizzas: Pizza[] = [
-  { name: "Cheese Lovers", toppings: "Classic Cheese Pizza", price: "129" },
-  { name: "Veg Treat", toppings: "Cheese, Onion, Capsicum", price: "129" },
-  {
-    name: "Mexican Delight",
-    toppings: "Cheese, Jalapeno, Golden Corn",
-    price: "129",
-  },
-  {
-    name: "Spicy Mushroom",
-    toppings: "Special Colli Garlic Sauce, Onion, Capsicum, Mushroom",
-    price: "169",
-  },
-  {
-    name: "Spicy Paneer",
-    toppings: "Special Colli Garlic Sauce, Onion, Capsicum, Paneer",
-    price: "169",
-  },
-  {
-    name: "Golden Choice",
-    toppings: "Cheese, Onion, Sweet Corn, Paneer",
-    price: "169",
-  },
-  {
-    name: "Mixicana Veg Wonder",
-    toppings: "Cheese, Onion, Capsicum, Tomatoes, Jalapenos",
-    price: "169",
-  },
-  {
-    name: "Paneer Makhni",
-    toppings: "Spl. Makhni Sauce, Onion, Capsicum, Paneer",
-    price: "199",
-  },
-  {
-    name: "Tandoori Veggie Delight",
-    toppings:
-      "Cheese, Onion, Capsicum, Mushroom, Red Pepper with Spl Tandoori Sauce",
-    price: "199",
-  },
-  {
-    name: "Veggie Lover",
-    toppings: "Cheese, Onion, Capsicum, Mushroom, Tomatoes",
-    price: "199",
-  },
-  {
-    name: "Tandoori Paneer",
-    toppings:
-      "Cheese, Onion, Capsicum, Red Pepper, Paneer with Spl. Tandoori Sauce",
-    price: "299",
-  },
-  {
-    name: "Margherita",
-    toppings: "Tomato, Mozzarella, Basil",
-    price: "149",
-  },
-  {
-    name: "Pepperoni Feast",
-    toppings: "Pepperoni, Cheese, Tomato Sauce",
-    price: "179",
-  },
-  {
-    name: "Hawaiian Delight",
-    toppings: "Ham, Pineapple, Cheese",
-    price: "169",
-  },
-  {
-    name: "BBQ Chicken",
-    toppings: "BBQ Sauce, Chicken, Red Onions",
-    price: "189",
-  },
-  {
-    name: "Supreme",
-    toppings: "Pepperoni, Sausage, Bell Peppers, Onions, Olives",
-    price: "199",
-  },
-  {
-    name: "Buffalo Chicken",
-    toppings: "Buffalo Sauce, Chicken, Ranch Dressing, Mozzarella",
-    price: "189",
-  },
-  {
-    name: "Mediterranean",
-    toppings: "Spinach, Feta Cheese, Olives, Sun-Dried Tomatoes",
-    price: "179",
-  },
-  {
-    name: "Four Cheese",
-    toppings: "Mozzarella, Cheddar, Gouda, Parmesan",
-    price: "169",
-  },
-  {
-    name: "Pesto Veggie",
-    toppings: "Pesto Sauce, Mushrooms, Tomatoes, Bell Peppers",
-    price: "179",
-  },
-  {
-    name: "Meat Lover's",
-    toppings: "Pepperoni, Sausage, Bacon, Ham",
-    price: "209",
-  },
-];
-
-// const images = [
-//   "https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   "https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   "https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-// ];
-
 export function Items() {
-  const pizzass = useQuery(api.pizzas.getPizzas);
+  const pizzas = useQuery(api.pizzas.getPizzas);
   const addtoCart = useMutation(api.cart.cartItem);
   const { user } = useUser();
-  const [itemCount, setItemCount] = useState<number[]>(
-    new Array(pizzas.length).fill(0)
-  );
-  const [cartItems, setCartItems] = useState<Pizza[]>([]);
-  const newCounts = [...itemCount];
+  const [itemCount, setItemCount] = useState<number[]>([]);
+  const [added, setAdded] = useState<boolean>(false);
 
-  const handleAddToCart = (index: number, pizzaId: string) => {
+  useEffect(() => {
+    if (pizzas) {
+      setItemCount(new Array(pizzas.length).fill(0));
+    }
+  }, [pizzas]);
+
+  const handleOneAddToCart = (index: number, pizzaId: string) => {
+    const newCounts = [...itemCount];
     newCounts[index]++;
+    setItemCount(newCounts);
+    if (newCounts[index] === 1) {
+      addtoCart({
+        userId: String(user?.id),
+        pizzaId: pizzaId,
+        quantity: 1,
+      });
+    }
+  };
+
+  const handleManyAddToCart = (index: number, pizzaId: string) => {
+    const newCounts = [...itemCount];
     setItemCount(newCounts);
     addtoCart({
       userId: String(user?.id),
       pizzaId: pizzaId,
-      quantity: 1,
+      quantity: newCounts[index],
     });
-    const pizza = pizzas[index];
-    setCartItems([...cartItems, pizza]);
-    // localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    setAdded(true);
   };
 
   const handleRemoveFromCart = (index: number) => {
+    const newCounts = [...itemCount];
     if (newCounts[index] > 0) {
       newCounts[index]--;
+      setItemCount(newCounts);
     }
-    setItemCount(newCounts);
-
-    const updatedCartItems = [...cartItems];
-    updatedCartItems.splice(index, 1);
-    setCartItems(updatedCartItems);
   };
 
   return (
     <div className="flex flex-wrap justify-center max-w-screen-xl mx-auto mt-20 sm:mt-24 md:mt-28 lg:mt-36">
-      {pizzass?.map((pizza, index) => (
+      {pizzas?.map((pizza, index) => (
         <CardContainer key={index} className="m-5">
           <CardBody className="bg-gray-50 tracking-tight md:tracking-wide relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[24rem] h-auto rounded-xl p-6 border">
             <CardItem
@@ -221,7 +117,7 @@ export function Items() {
               >
                 {itemCount[index] === 0 ? (
                   <div
-                    onClick={() => handleAddToCart(index, pizza._id)}
+                    onClick={() => handleOneAddToCart(index, pizza._id)}
                     className="text-sm font-Annapura"
                   >
                     Add to Cart →
@@ -232,13 +128,17 @@ export function Items() {
                       onClick={() => handleRemoveFromCart(index)}
                       className="hover:bg-secondary rounded-md p-1 cursor-pointer"
                     >
-                      <Minus size={20} />
+                      {itemCount[index] === 1 ? (
+                        <Trash2 size={20} />
+                      ) : (
+                        <Minus size={20} />
+                      )}
                     </div>
                     <Separator orientation="vertical" className="bg-primary" />
                     <div>{itemCount[index]}</div>
                     <Separator orientation="vertical" className="bg-primary" />
                     <div
-                      onClick={() => handleAddToCart(index, pizza._id)}
+                      onClick={() => handleOneAddToCart(index, pizza._id)}
                       className="hover:bg-secondary rounded-md p-1 cursor-pointer"
                     >
                       <Plus size={20} />
@@ -246,13 +146,30 @@ export function Items() {
                   </div>
                 )}
               </CardItem>
-              <CardItem
-                translateZ={20}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold font-Anta"
-              >
-                Buy Now
-              </CardItem>
+              {itemCount[index] === 0 || itemCount[index] === 1 ? (
+                <div className="w-full">
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold font-Anta"
+                  >
+                    Buy Now
+                  </CardItem>
+                </div>
+              ) : (
+                <div
+                  onClick={() => handleManyAddToCart(index, pizza._id)}
+                  className="w-full"
+                >
+                  <CardItem
+                    translateZ={20}
+                    as="button"
+                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold font-Anta"
+                  >
+                    Add {itemCount[index]} To Cart
+                  </CardItem>
+                </div>
+              )}
             </div>
           </CardBody>
         </CardContainer>
